2c04bf3a8efbe481ab977c5100d8a0acbdbdc3d6
Scott McElwain <sdmcelwain@proton.me>
2024-12-12 10:22:38 -0500

    0203-02 submitted

---
+++ CH02/0203-02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Please enter your first and second names\n";
+    string first;
+    string second;
+    cin >> first >> second;
+    cout << "Hello, " << first << " " << second << '\n';
+}
