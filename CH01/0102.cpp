b084c80b074631b47c8b5d0ddf6222a10fd3564d
Scott McElwain <sdmcelwain@proton.me>
2024-12-11 21:32:43 -0500

    CH01/0102.cpp

---
+++ CH01/0102.cpp
@@ -0,0 +1,13 @@
+// SDM 11 December 2024 -- see notes below
+// tested on Ubuntu with gcc 13.2.0:  g++ -o 0102 0102.cpp 
+
+// at this time only msvc supports import std;
+// iostream included for std::cout support instead
+
+//import std;
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
