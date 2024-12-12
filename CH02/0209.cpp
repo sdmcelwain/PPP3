4f156f6cb51690fe98d24650d64c3b8da4782265
sdmcelwain <sdmcelwain@proton.me>
2024-12-12 21:09:19 +0000

    0209.cpp added

---
+++ CH02/0209.cpp
@@ -0,0 +1,21 @@
+// Seems like the author made an error at this line:
+// << " c==" << c
+// Not in errata at this time, however.
+// Try input of 65 for the ASCII 'A' character
+// Not sure what he intended; revert to original code if desired
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d = 0;
+    while (cin >> d) {
+        int i = d;
+        char c = i;
+        cout << "d==" << d
+            << " i==" << i
+            //<< " c==" << c
+            << " c==" << (int)c
+            << " char(" << c << ")\n";
+    }
+}
