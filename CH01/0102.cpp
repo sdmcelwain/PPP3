98c9893c789004be35776c2c9827007f2e598689
sdmcelwain <sdmcelwain@proton.me>
2024-12-12 06:05:17 -0500

    Update 0102.cpp

--- CH01/0102.cpp
+++ CH01/0102.cpp
@@ -1,9 +1,3 @@
-// SDM 11 December 2024 -- see notes below
-// tested on Ubuntu with gcc 13.2.0:  g++ -o 0102 0102.cpp 
-
-// at this time only msvc supports import std;
-// iostream included for std::cout support instead
-
 //import std;
 #include <iostream>
 
