cb7295a8a2f879025fd4f89dbdd8ee3b92e7ac00
sdmcelwain <sdmcelwain@proton.me>
2024-12-12 19:14:15 -0500

    Update 0209.cpp

--- CH02/0209.cpp
+++ CH02/0209.cpp
@@ -1,9 +1,3 @@
-// Seems like the author made an error at this line:
-// << " c==" << c
-// Not in errata at this time, however.
-// Try input of 65 for the ASCII 'A' character
-// Not sure what he intended; revert to original code if desired
-
 #include <iostream>
 using namespace std;
 
@@ -14,9 +8,7 @@ int main() {
         char c = i;
         cout << "d==" << d
             << " i==" << i
-            //<< " c==" << c                 //original text
-            //<< " c==" << (int)c            //old syle cast
-            << " c==" << static_cast<int>(c) //modern
+            << " c==" << c
             << " char(" << c << ")\n";
     }
 }
